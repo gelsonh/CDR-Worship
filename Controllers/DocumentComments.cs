6d8b080afdb1f5c522016fa931866932a07b092d
Gelson Hernandez <gelsonpf49@hotmail.com>
2024-10-28 21:53:17 -0400

    Added Model DocumentComments

--- Controllers/DocumentComments.cs
+++ Controllers/DocumentComments.cs
@@ -1,60 +1,140 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using CDR_Worship.Models;
-// using CDR_Worship.Data;
-// using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CDR_Worship.Models;
+using CDR_Worship.Services.Interfaces;
+using CDR_Worship.Models.Enums;
+using CDR_Worship.Data;
+using Microsoft.EntityFrameworkCore;
 
-// namespace CDR_Worship.Controllers
-// {
-//     public class DocumentCommentsController : Controller
-//     {
-//         private readonly ApplicationDbContext _context;
-//         private readonly UserManager<AppUser> _userManager;
+namespace CDR_Worship.Controllers
+{
+    public class DocumentCommentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ICommentService _commentService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IImageService _imageService;
+        private readonly IDateTimeService _dateTimeService;
 
-//         public DocumentCommentsController(ApplicationDbContext context, UserManager<AppUser> userManager)
-//         {
-//             _context = context;
-//             _userManager = userManager;
-//         }
+        public DocumentCommentsController(ApplicationDbContext context, ICommentService commentService, UserManager<AppUser> userManager, IImageService imageService, IDateTimeService dateTimeService)
+        {
+            _context = context;
+            _commentService = commentService;
+            _userManager = userManager;
+            _imageService = imageService;
+            _dateTimeService = dateTimeService;
+        }
 
-//         [HttpGet]
-//         public async Task<IActionResult> Index()
-//         {
-//             var comments = await _context.DocumentComments
-//                 .Include(c => c.User)
-//                 .OrderByDescending(c => c.Created)
-//                 .ToListAsync();
-//             return View(comments);
-//         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DocumentComment model)
+{
+    if (!ModelState.IsValid)
+    {
+        return BadRequest();
+    }
 
-//         [HttpPost]
-//         [ValidateAntiForgeryToken]
-//         public async Task<IActionResult> Create(DocumentComment documentComment)
-//         {
-//             if (ModelState.IsValid)
-//             {
-//                 var user = await _userManager.GetUserAsync(User);
-//                 if (user != null)
-//                 {
-//                     documentComment.AppUserId = user.Id;
-//                     documentComment.Created = DateTime.UtcNow;
-//                     _context.DocumentComments.Add(documentComment);
-//                     await _context.SaveChangesAsync();
-//                     return RedirectToAction(nameof(Index));
-//                 }
-//                 ModelState.AddModelError("", "User not found.");
-//             }
-//             return View(documentComment);
-//         }
+    var userId = _userManager.GetUserId(User);
 
-//         [HttpGet]
-//         public async Task<IActionResult> GetComments()
-//         {
-//             var comments = await _context.DocumentComments
-//                 .Include(c => c.User)
-//                 .OrderByDescending(c => c.Created)
-//                 .ToListAsync();
-//             return PartialView("_DocumentCommentPartial", comments);
-//         }
-//     }
-// }
+    // Crear el nuevo comentario
+    var comment = new DocumentComment
+    {
+        Comment = model.Comment,
+        ScheduledSongId = model.ScheduledSongId,
+        Created = DateTime.UtcNow,
+        AppUserId = userId
+    };
+
+    await _commentService.AddCommentAsync(comment);
+
+ // Convertir la fecha a la hora local del servidor antes de enviarla al frontend
+    var localTime = TimeZoneInfo.ConvertTimeFromUtc(comment.Created, TimeZoneInfo.Local); // Correcto uso de la conversión a hora local
+
+    // Formatear la fecha usando el servicio
+    // var formattedDate = _dateTimeService.FormatCommentDate(comment.Created);
+
+    // Obtener los datos de la imagen del usuario
+    var user = await _userManager.FindByIdAsync(userId!);
+    var userImage = _imageService.ConvertByteArrayToFile(user?.ImageFileData, user?.ImageFileType, DefaultImage.UserImage);
+
+    // Devolver la respuesta con la fecha formateada
+    return Ok(new
+    {
+        commentId = comment.Id,
+        commentText = comment.Comment,
+        commentCreated = localTime.ToString("MM-dd HH:mm"),  // Usar la fecha formateada
+        commentUserName = user?.FirstName,
+        commentUserImage = userImage
+    });
+}
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LikeComment([FromBody] CommentLike model)
+    {
+    var userId = _userManager.GetUserId(User);
+
+    var likeAdded = await _commentService.AddLikeAsync(model.DocumentCommentId, userId!);
+
+    // Obtenemos el comentario actualizado para obtener el número de likes
+    var comment = await _context.DocumentComments.FirstOrDefaultAsync(c => c.Id == model.DocumentCommentId);
+
+    // Obtenemos todos los usuarios que han dado like para el tooltip
+    var likedUsers = await _context.CommentLikes
+        .Where(cl => cl.DocumentCommentId == model.DocumentCommentId)
+        .Select(cl => cl.User!.FirstName)
+        .ToListAsync();
+
+    return Ok(new 
+    {
+        success = likeAdded,
+        likes = comment?.LikesCount,  // Número de likes actual
+        likedUsers  // Lista de nombres para el tooltip
+    });
+}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ReplyComment([FromBody] CommentReply model)
+    {   
+    if (model != null && !string.IsNullOrEmpty(model.ReplyText))
+    {
+        var parentComment = await _commentService.GetCommentByIdAsync(model.ParentCommentId);
+        if (parentComment != null)
+        {
+            var reply = new DocumentComment
+            {
+                Comment = model.ReplyText,
+                Created = DateTime.UtcNow,
+                ParentCommentId = model.ParentCommentId,
+                ScheduledSongId = model.ScheduledSongId,
+                AppUserId = _userManager.GetUserId(User)
+            };
+
+            await _commentService.AddCommentAsync(reply);
+
+            // Obtener los datos del usuario y la imagen
+            var user = await _userManager.FindByIdAsync(reply.AppUserId!);
+            var userImage = _imageService.ConvertByteArrayToFile(user?.ImageFileData, user?.ImageFileType, DefaultImage.UserImage);
+
+            // Convertir la fecha a la hora local del servidor antes de enviarla al frontend
+            var localTime = TimeZoneInfo.ConvertTimeFromUtc(reply.Created, TimeZoneInfo.Local); // Correcto uso de la conversión a hora local
+
+            // Formatear la fecha de creación de la respuesta usando el servicio DateTimeService
+            // var formattedDate = _dateTimeService.FormatCommentDate(reply.Created);
+
+            return Ok(new
+            {
+                replyId = reply.Id,
+                replyText = reply.Comment,
+                replyUserName = user?.FirstName,
+                replyCreated = localTime.ToString("MM-dd HH:mm"),  // Usar la fecha formateada
+                replyUserImage = userImage
+            });
+        }
+    }
+    return BadRequest();
+}
+
+    }
+}
